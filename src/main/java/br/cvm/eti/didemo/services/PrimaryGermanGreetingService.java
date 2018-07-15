227f3b80be64a188372eb6ba6ccb61d4b4a97b32
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2018-07-15 10:52:51 -0300

    Section 3.37
    - Default Profile

--- src/main/java/br/cvm/eti/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/br/cvm/eti/didemo/services/PrimaryGermanGreetingService.java
@@ -5,12 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"en","default"})
 @Primary
-public class PrimaryGreetingService implements GreetingService {
-
-    @Override
-    public String sayGreeting() {
-        return "Hello - Primary Greeting service";
-    }
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
+	@Override
+	public String sayGreeting() {
+		return "Primärer Grußdienst";
+	}
 }
