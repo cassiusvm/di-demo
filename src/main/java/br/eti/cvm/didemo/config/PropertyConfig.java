425729f01253f57b565ea8db46d7d2dec5ef0e57
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2018-07-17 10:09:57 -0300

    Section 6.73
    - Spring Environment Properties

--- src/main/java/br/eti/cvm/didemo/config/PropertyConfig.java
+++ src/main/java/br/eti/cvm/didemo/config/PropertyConfig.java
@@ -1,16 +1,21 @@
 package br.eti.cvm.didemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import br.eti.cvm.didemo.examplebeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+	@Autowired
+    Environment env;
+	
 	@Value("${guru.username}")
 	String username;
 
@@ -23,7 +28,8 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUsername(username);
+//		fakeDataSource.setUsername(username);
+		fakeDataSource.setUsername(env.getProperty("USERNAME"));
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setDburl(dburl);
 		return fakeDataSource;
