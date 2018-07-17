7d2408e7dd84f9ee02fa1715f47cdd0c38a69849
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2018-07-17 10:38:32 -0300

    Section 6.75
    - Spring Boot Application.properties

--- src/main/java/br/eti/cvm/didemo/config/PropertyConfig.java
+++ src/main/java/br/eti/cvm/didemo/config/PropertyConfig.java
@@ -1,26 +1,14 @@
 package br.eti.cvm.didemo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 import br.eti.cvm.didemo.examplebeans.FakeDataSource;
 import br.eti.cvm.didemo.examplebeans.FakeJmsBroker;
 
 @Configuration
-// Spring 5
-//@PropertySource("classpath:datasource.properties")
-// Spring 4
-@PropertySources({ @PropertySource("classpath:datasource.properties"), @PropertySource("classpath:jms.properties") })
 public class PropertyConfig {
-	@Autowired
-	Environment env;
-
 	@Value("${guru.username}")
 	String username;
 
@@ -29,21 +17,20 @@ public class PropertyConfig {
 
 	@Value("${guru.dburl}")
 	String dburl;
-	
+
 	@Value("${guru.jms.username}")
-    String jmsUsername;
+	String jmsUsername;
 
-    @Value("${guru.jms.password}")
-    String jmsPassoword;
+	@Value("${guru.jms.password}")
+	String jmsPassoword;
 
-    @Value("${guru.jms.url}")
-    String jmsUrl;
+	@Value("${guru.jms.url}")
+	String jmsUrl;
 
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-//		fakeDataSource.setUsername(username);
-		fakeDataSource.setUsername(env.getProperty("USERNAME"));
+		fakeDataSource.setUsername(username);
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setDburl(dburl);
 		return fakeDataSource;
@@ -56,11 +43,5 @@ public class PropertyConfig {
 		jmsBroker.setPassword(jmsPassoword);
 		jmsBroker.setUrl(jmsUrl);
 		return jmsBroker;
-	}
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		return propertySourcesPlaceholderConfigurer;
 	}
 }
